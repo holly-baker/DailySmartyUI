cdcc8c6302d2758350ea56608ee250b7bf5947a1
Holly Baker <hmaybaker@icloud.com>
2020-03-28 18:36:42 -0400

    set up logo component and corresponding styles

---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class logo extends Component {
+    render() {
+        return (
+            <div className="logo-main"> 
+                <img alt="daily smarty ui image logo big" src="/assets/ds_circle_logo.png"/>
+            </div>
+        )
+    }
+}
+
+export default logo;
