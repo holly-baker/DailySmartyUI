b0f188cb99cc5d3992648bb205a6be197a8763be
Holly Baker <hmaybaker@icloud.com>
2020-03-31 00:33:29 -0400

    connect recent posts to redux and set up redux-thunk for api requests

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import thunk from 'redux-thunk';
+
 import reducers from "./reducers";
 import Home from "./components/home";
 import Results from './components/results';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 import "./style/main.scss";
 
