acb50f889df39523edec25d6164dc503d8da68fd
Holly Baker <hmaybaker@icloud.com>
2020-04-08 22:54:13 -0400

    styled the results component search bar

--- src/components/results.js
+++ src/components/results.js
@@ -15,7 +15,7 @@ class Results extends Component {
         return (
             <div className="results">
                 <Logo size={55}/>
-                <SearchBar onSubmit={(query) => this.handleSearchBarSubmit}/>
+                <SearchBar page="results" onSubmit={(query) => this.handleSearchBarSubmit}/>
                 <ResultsPosts/>
             </div>
         )
